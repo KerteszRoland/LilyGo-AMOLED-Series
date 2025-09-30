a621cd5db2e74ef64afad96e9e4d6a7bda30341f
Roland Kertész <roland.kertesz@itsquad.hu>
2025-09-30 21:23:47 +0200

    set up

--- examples/lvgl_version_9/lvgl_version_9.ino
+++ examples/lvgl_version_9/lvgl_version_9.ino
@@ -4,15 +4,16 @@
  * @license   MIT
  * @copyright Copyright (c) 2025  Shenzhen Xinyuan Electronic Technology Co., Ltd
  * @date      2025-02-27
- * @note      This sketch is adapted to lvgl9 version. Please upgrade lvgl to lvgl 9 version, 
+ * @note      This sketch is adapted to lvgl9 version. Please upgrade lvgl to lvgl 9 version,
  *            and rename <LilyGo-AMOLED-Series/src/lv_conf.h> to <lv_conf.h.v8> and <lv_conf.h.v9> to <lv_conf.h>
  */
 #include <LilyGo_AMOLED.h>
 #include <LV_Helper.h>
+#include "esp_heap_caps.h"
 
 LilyGo_Class amoled;
 
-static void set_angle(void * obj, int32_t v)
+static void set_angle(void *obj, int32_t v)
 {
     lv_arc_set_value((lv_obj_t *)obj, v);
 }
@@ -24,20 +25,21 @@ void setup()
     bool rslt = false;
 
     // Begin LilyGo  1.47 Inch AMOLED board class
-    //rslt = amoled.beginAMOLED_147();
-
+    // rslt = amoled.beginAMOLED_147();
 
     // Begin LilyGo  1.91 Inch AMOLED board class
-    //rslt =  amoled.beginAMOLED_191();
+    // rslt =  amoled.beginAMOLED_191();
 
     // Begin LilyGo  2.41 Inch AMOLED board class
-    //rslt =  amoled.beginAMOLED_241();
+    // rslt =  amoled.beginAMOLED_241();
 
     // Automatically determine the access device
     rslt = amoled.begin();
 
-    if (!rslt) {
-        while (1) {
+    if (!rslt)
+    {
+        while (1)
+        {
             Serial.println("The board model cannot be detected, please raise the Core Debug Level to an error");
             delay(1000);
         }
@@ -45,14 +47,13 @@ void setup()
 
     beginLvglHelper(amoled);
 
-
 #if LVGL_VERSION_MAJOR == 9
     /*Create an Arc*/
     lv_obj_t *arc = lv_arc_create(lv_screen_active());
     lv_arc_set_rotation(arc, 270);
     lv_arc_set_bg_angles(arc, 0, 360);
     lv_obj_remove_style(arc, NULL, LV_PART_KNOB);   /*Be sure the knob is not displayed*/
-    lv_obj_remove_flag(arc, LV_OBJ_FLAG_CLICKABLE);  /*To not allow adjusting by click*/
+    lv_obj_remove_flag(arc, LV_OBJ_FLAG_CLICKABLE); /*To not allow adjusting by click*/
     lv_obj_center(arc);
 
     lv_anim_t a;
@@ -60,16 +61,15 @@ void setup()
     lv_anim_set_var(&a, arc);
     lv_anim_set_exec_cb(&a, set_angle);
     lv_anim_set_duration(&a, 1000);
-    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);    /*Just for the demo*/
+    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE); /*Just for the demo*/
     lv_anim_set_repeat_delay(&a, 500);
     lv_anim_set_values(&a, 0, 100);
     lv_anim_start(&a);
 
-
-    lv_obj_t *label = lv_label_create( lv_screen_active() );
-    lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
-    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
-    lv_obj_align_to(label, arc, LV_ALIGN_OUT_BOTTOM_MID, 0, 0 );
+    lv_obj_t *label = lv_label_create(lv_screen_active());
+    lv_label_set_text(label, "Hello Arduino, I'm LVGL!");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align_to(label, arc, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
 
     static lv_style_t style_bg;
     static lv_style_t style_indic;
@@ -87,20 +87,20 @@ void setup()
     lv_style_set_radius(&style_indic, 3);
 
     lv_obj_t *bar = lv_bar_create(lv_screen_active());
-    lv_obj_remove_style_all(bar);  /*To have a clean start*/
+    lv_obj_remove_style_all(bar); /*To have a clean start*/
     lv_obj_add_style(bar, &style_bg, 0);
     lv_obj_add_style(bar, &style_indic, LV_PART_INDICATOR);
 
     lv_obj_set_size(bar, 200, 20);
-    lv_obj_align_to(bar, label, LV_ALIGN_OUT_BOTTOM_MID, 0, 0 );
+    lv_obj_align_to(bar, label, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
     lv_bar_set_value(bar, 100, LV_ANIM_ON);
 #endif
 }
 
-
 void loop()
 {
 #if LVGL_VERSION_MAJOR == 9
+    heap_caps_check_integrity_all(true);
     lv_task_handler();
     delay(5);
 #else
